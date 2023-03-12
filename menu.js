6e1c1a824d668e46831f97bd1588105e30af24b2
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-03-12 18:10:33 +0000

    update
    Inseri o tema claro no artigo, além de outras alterações

--- menu.js
+++ menu.js
@@ -48,3 +48,79 @@ if(target.length){
 })
     
 }
+
+
+
+function mudarTemaEscuro() {
+
+    let temaclaro = document.getElementById('temaBotaoClaro')
+    let temaescuro = document.getElementById('temaBotaoEscuro')
+    let body = document.getElementById('bodyArticle')
+    let artigo = document.getElementById('articleOrigem')
+    let h2 = document.getElementById('tituloArtigo')
+    let p = document.querySelectorAll('p')
+    let legenda = document.querySelectorAll('legend')
+    let autor = document.getElementById('autor')
+    let linkAutor = document.getElementById('linkAutor')
+    let logoRodape = document.getElementById('logoClube')
+    let pSessao = document.getElementById('pSessao')
+    let pRodape = document.getElementById('pRodape')
+    let pDev = document.getElementById('pDev')
+    
+    
+     p.forEach((p) => {
+        p.style.color = 'black'
+    } )
+
+    legenda.forEach((legend) => {
+        legend.style.color = 'black'
+    } )
+
+    temaescuro.style.display = 'block'
+    temaclaro.style.display = 'none'
+    body.style.background = 'white'
+    artigo.style.background = 'white'
+    h2.style.color = 'black'
+    autor.style.color = 'black'
+    linkAutor.style.color = 'black'
+    logoRodape.style.background = "white"
+    pSessao.style.color = 'black'
+    pRodape.style.color = 'black'
+    pDev.style.color = 'black'
+
+}
+
+function mudarTemaClaro() {
+
+    let temaclaro = document.getElementById('temaBotaoClaro')
+    let temaescuro = document.getElementById('temaBotaoEscuro')
+    let body = document.getElementById('bodyArticle')
+    let artigo = document.getElementById('articleOrigem')
+    let h2 = document.getElementById('tituloArtigo')
+    let p = document.querySelectorAll('p')
+    let legenda = document.querySelectorAll('legend')
+    let autor = document.getElementById('autor')
+    let linkAutor = document.getElementById('linkAutor')
+    let pSessao = document.getElementById('pSessao')
+    let pRodape = document.getElementById('pRodape')
+    let pDev = document.getElementById('pDev')
+
+    p.forEach((p) => {
+        p.style.color = 'white'
+    } )
+
+    legenda.forEach((legend) => {
+        legend.style.color = 'white'
+    } )
+
+    temaescuro.style.display = 'none'
+    temaclaro.style.display = 'block'
+    body.style.background = 'black'
+    artigo.style.background = 'black'
+    h2.style.color = 'white'
+    autor.style.color = 'white'
+    linkAutor.style.color = 'white'
+    pSessao.style.color = 'black'
+    pRodape.style.color = 'black'
+    pDev.style.color = 'black'
+}
