1eef6ac2d936d9c88ade3d5065e6fd1ae3af4844
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-03-12 10:43:35 +0000

    Mudanças nas fotos, texto e menu
    Adicionei animação nas imagens ao fazer scroll na página e alterei algumas coisas no texto

--- menu.js
+++ menu.js
@@ -1,12 +1,50 @@
 function clickMenu() {
     
     var menu = document.getElementById('menu')
+    var icone = document.getElementById('navigation')
 
     menu.classList.toggle('active')
+    
+    if(menu.className == ('active')) {
+        icone.style.backgroundImage = "url(imagens/navigationfundo2.png)"
+    } else {
+        icone.style.backgroundImage = "url(imagens/navigationfundo.png)"
+    }
+    
+
 }
 
 function mudarTamanho() {
     if (window.innerWidth >= 1200) {
         menu.style.visibility = 'visible'
     }
+}
+
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate';
+
+
+function animeScroll() {
+
+const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4)
+target.forEach(function(element){
+if ((windowTop) > element.offsetTop) {
+    element.classList.add(animationClass);
+} else {
+    element.classList.remove(animationClass);
+}
+
+})
+
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', function(){
+
+    animeScroll();
+
+})
+    
 }
